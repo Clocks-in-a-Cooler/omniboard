29809d1f3dd6fad38e9eb8fcc903e94963909840
clocks-in-a-cooler <34520271+Clocks-in-a-cooler@users.noreply.github.com>
2020-01-27 16:06:39 -0500

    working on the UI, got some of it down. more to come.

--- webpage/whiteboard_client.js
+++ webpage/whiteboard_client.js
@@ -1,19 +1,20 @@
 var form = document.querySelector("form");
-var io = io();
+/*var io = io();
 
 form.addEventListener("submit", function(event) {
     event.preventDefault();
     console.log("name: " + form.username.value);
     io.emit("login", { name: form.username.value });
     //...and we wait for the reply from the server
-});
+});*/
 
 var image_layer = document.getElementById("image_layer");
 var image_cxt = image_layer.getContext("2d");
 var control_layer = document.getElementById("control_layer");
 var control_cxt = control_layer.getContext("2d");
 
-image_layer.width = control_layer.width = window.innerWidth;
+image_layer.width = control_layer.width = window.innerWidth - 400;
 image_layer.height = control_layer.height = window.innerHeight;
 
-
+var chat = document.getElementById("chat");
+//chat.style.height = window.innerHeight + "px";
