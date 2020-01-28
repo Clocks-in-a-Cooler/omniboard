7f79d1e5c0bd2f2445fb896977b3542eea6c0166
clocks-in-a-cooler <34520271+Clocks-in-a-cooler@users.noreply.github.com>
2020-01-27 21:23:47 -0500

    finishing touches to the client UI. hard work!

--- webpage/whiteboard_client.js
+++ webpage/whiteboard_client.js
@@ -1,12 +1,12 @@
 var form = document.querySelector("form");
-/*var io = io();
+var io = io();
 
 form.addEventListener("submit", function(event) {
     event.preventDefault();
     console.log("name: " + form.username.value);
     io.emit("login", { name: form.username.value });
     //...and we wait for the reply from the server
-});*/
+});
 
 var image_layer = document.getElementById("image_layer");
 var image_cxt = image_layer.getContext("2d");
@@ -17,4 +17,3 @@ image_layer.width = control_layer.width = window.innerWidth - 400;
 image_layer.height = control_layer.height = window.innerHeight;
 
 var chat = document.getElementById("chat");
-//chat.style.height = window.innerHeight + "px";
